75f547fba69e39a971575a1e1883643708c4f2fe
Mark <hummel.marcus@icloud.com>
2022-12-04 00:29:53 -0500

    minor update

--- Exceptions/Driver.cpp
+++ Exceptions/Driver.cpp
@@ -16,13 +16,13 @@ int main() {
 	char arr[52]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 	try {
-		cout << character(arr, 'A', -1) << endl;
+		cout << character(arr, 'A', 1) << endl;
 	}
 	catch(invalidRangeException){
-		cout << "The offset is out of range";
+		cout << "The offset is out of range" << endl;
 	}
 	catch (invalidCharacterException) {
-		cout << "The character is out of range";
+		cout << "The character is out of range" << endl;
 	}
 	system("pause");
 
