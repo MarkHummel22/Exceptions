74d50a177eb498498d2669775a2dd3e6ab044d4e
Mark <hummel.marcus@icloud.com>
2022-12-04 00:35:19 -0500

    added exception to catch the switch from upper case to lower case

--- Exceptions/Driver.cpp
+++ Exceptions/Driver.cpp
@@ -9,6 +9,7 @@
 using namespace std;
 class invalidRangeException {};
 class invalidCharacterException{};
+class invalidCaseChangeException{};
 char character(char[], char, int);
 
 
@@ -16,13 +17,16 @@ int main() {
 	char arr[52]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 	try {
-		cout << character(arr, 'A', 1) << endl;
+		cout << character(arr, 'Z', 1) << endl;
 	}
 	catch(invalidRangeException){
 		cout << "The offset is out of range" << endl;
 	}
 	catch (invalidCharacterException) {
 		cout << "The character is out of range" << endl;
+	}
+	catch (invalidCaseChangeException) {
+		cout << "Value out of bounds" << endl;
 	}
 	system("pause");
 
@@ -49,6 +53,11 @@ char character(char arr[52], char start, int offset) {
 	}
 	else
 		throw invalidCharacterException();
+	if (start >= 'A' && start <= 'Z') {
+		if (output >= 'a' && output <= 'z') {
+			throw invalidCaseChangeException();
+		}
+	}
 	return output;
 }
 
