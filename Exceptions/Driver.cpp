7c297dbed9aac893b28775554f1ba8509af5f112
Mark <hummel.marcus@icloud.com>
2022-12-03 21:06:58 -0500

    set up array to call from

---
+++ Exceptions/Driver.cpp
@@ -0,0 +1,30 @@
+// Mark Hummel
+// CIS 1202
+// 12/3/22
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+char character(char, int);
+
+int main() {
+
+
+	char character();
+
+
+	return 0;
+}
+
+
+
+char character(char start, int offset) {
+	char out = ' ';
+	char arr[52]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z'};
+
+
+
+	return out;
+}
