ca037eed6b0b21b3597b7f3e0c264838a988d77e
Mark <hummel.marcus@icloud.com>
2022-12-03 22:29:00 -0500

    Tried a couple different approaches and landed on one that works. linear search through array with catch blocks

---
+++ Exceptions/Alphabet.h
@@ -0,0 +1,18 @@
+#pragma once
+#ifndef ALPHABET_H
+#define ALPHABET_H
+
+class ABC
+{
+private:
+	
+public:
+	class invalidRangeException{};
+
+	class invalidCharacterException{};
+
+	
+};
+
+
+#endif // !ALPHABET_H
--- Exceptions/Driver.cpp
+++ Exceptions/Driver.cpp
@@ -4,27 +4,44 @@
 
 #include <iostream>
 #include <string>
+#include "Alphabet.h"
 
 using namespace std;
+class invalidRangeException {};
+char character(char[], char, int);
 
-char character(char, int);
 
 int main() {
+	char arr[52]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
-
-	char character();
-
+	try {
+		cout << character(arr, 'A', 5) << endl;
+	}
+	catch(invalidRangeException){
+		cout << "The offset is out of range";
+	}
+	system("pause");
 
 	return 0;
 }
 
 
-
-char character(char start, int offset) {
-	char out = ' ';
-	char arr[52]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v','w', 'x', 'y', 'z'};
-
-
-
-	return out;
+char character(char arr[52], char start, int offset) {
+	int i = 0;
+	bool found = false;
+	char output;
+	if (start >= 'A' && start <= 'z') {
+		while (i < 52 && !found) {
+			if (arr[i] == start) {
+				found = true;
+				output = i;
+			}
+			i++;
+		}
+		output = arr[(i - 1) + offset];
+	}
+	else
+		throw invalidRangeException();
+	return output;
 }
+
