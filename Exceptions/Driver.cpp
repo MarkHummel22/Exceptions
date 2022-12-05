199eeae6c1facda7c9399f5ed4076bfdf9c62b25
Mark <hummel.marcus@icloud.com>
2022-12-04 19:36:05 -0500

    cleaned up unused elements

--- Exceptions/Driver.cpp
+++ Exceptions/Driver.cpp
@@ -17,7 +17,7 @@ int main() {
 	char arr[52]{ 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z' };
 
 	try {
-		cout << character(arr, 'Z', 1) << endl;
+		cout << character(arr, 'H', 12) << endl;
 	}
 	catch(invalidRangeException){
 		cout << "The offset is out of range" << endl;
